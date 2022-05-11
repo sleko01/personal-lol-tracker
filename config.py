fa16a42c711ba7b680733b57067b859627dca6bf
sleko01 <svenleko2@gmail.com>
2022-05-11 11:57:24 +0200

    Changed api_key

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class APIFetcher:
-    api_key = 'RGAPI-7b853924-bcf6-4102-b043-ce3cd689b8ae'
+    api_key = 'RGAPI-ec6c5f59-cbe1-422b-b90b-df7e748a12c5'
     puuid = 'vRcJBT69hPYI4W5NnxWnDsKY-bI0e1Dih7gTAx5KHzGkZtIJXQ9wjxAWMTTTzrIkF-87544a9GCAvA'
 
     # api_key and puuid are class attributes since they are used everywhere
